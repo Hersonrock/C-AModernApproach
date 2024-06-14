8c79a43ee832135520ac14e4c28179a7f6d70b93
Hersonrock <hjpg20@gmail.com>
2024-06-14 02:06:57 -0500

    removing magic numbers for radius limit

--- ch2/project_3/main.c
+++ ch2/project_3/main.c
@@ -3,6 +3,7 @@
 
 #define PI 3.141592
 #define MAX_SIZE 4
+#define LIMIT 99
 
 int main(int argc, char *argv[argc + 1]){
 
@@ -10,7 +11,7 @@ int main(int argc, char *argv[argc + 1]){
         float volume;
         char input[MAX_SIZE];
 
-        printf("What's the sphere radius?(1-99): ");
+        printf("What's the sphere radius?(1-%d): ", LIMIT);
         if(!fgets(input, MAX_SIZE, stdin)){
                 fprintf(stderr, "Error, invalid input\n");
                 return EXIT_FAILURE;
@@ -18,7 +19,7 @@ int main(int argc, char *argv[argc + 1]){
         }
         
         radius = strtod(input, NULL);
-        if(radius <= 99 && radius > 0){
+        if(radius <= LIMIT && radius > 0){
                 volume = (4.0f/3.0f) * PI * radius * radius * radius;
                 printf("A sphere with radius %.2fm"
                                 " has volume %.2fm^3 \n", 
@@ -29,7 +30,7 @@ int main(int argc, char *argv[argc + 1]){
 
         }
         else{
-                fprintf(stderr,"Error, radius has to be between 1-99\n");
+                fprintf(stderr,"Error, radius has to be between 1-%d\n", LIMIT);
                 return EXIT_FAILURE;
         }
         
