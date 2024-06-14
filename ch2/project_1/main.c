d6aacd6669f112fded19f514cb820f0637e98312
Hersonrock <hjpg20@gmail.com>
2024-06-14 01:18:25 -0500

    Finished Project 2.1

---
+++ ch2/project_1/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[argc + 1]){
+        /* printing
+         *         *
+         *        *
+         *       *
+         *  *   *
+         *   * *
+         *    *
+         *
+         */
+        
+        printf("        *\n");
+        printf("       *\n");
+        printf("      *\n");
+        printf(" *   *\n");
+        printf("  * *\n");
+        printf("   *\n");
+
+        return EXIT_SUCCESS;
+}
