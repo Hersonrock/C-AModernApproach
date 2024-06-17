0e0ae2bda7f5f0ed143e18de224e674a999b813d
Hersonrock <hjpg20@gmail.com>
2024-06-17 11:56:35 -0500

    Finished Project 5.5

---
+++ ch5/project_5/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SIZE 20
+int main(int argc, char *argv[argc + 1]){
+        
+        char input[SIZE];
+        float income, due;
+         
+        printf("Enter Income: ");
+        fgets(input, SIZE, stdin);
+        income = strtof(input, NULL);
+       
+        if(income < 750.0f){due = income * 0.01f;}
+        else if(income < 2250.0f){due = 7.50f + (income - 750.0f) * 0.02f;}
+        else if(income < 3750.0f){due = 37.50f + (income - 2250.0f) * 0.03f;}
+        else if(income < 5250.0f){due = 82.50f + (income - 3750.0f) * 0.04f;}
+        else if(income < 7000.0f){due = 142.50f + (income - 5250.0f) * 0.05f;}
+        else if(income >= 7000.0f){due = 230.00f + (income - 7000.0f) * 0.06f;}
+
+        printf("Tax due: %.2f\n", due);
+
+        return EXIT_SUCCESS;
+}
