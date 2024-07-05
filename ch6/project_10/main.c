bece3fb73ff88e4b4bf3e2e61ce41e283930e460
Hersonrock <hjpg20@gmail.com>
2024-07-04 22:37:43 -0500

    Finished Project 6.10

--- ch6/project_10/main.c
+++ ch6/project_10/main.c
@@ -7,13 +7,13 @@
 
 enum identifier {MONTH, DAY, YEAR};
 
+
 int main(int argc, char *argv[]){
 
         char buffer[SIZE];
-        char date[MAX_INPUTS][3] = {};
+        int date[MAX_INPUTS][3] = {};
         char *endptr;
         int counter = 0 ;
-        int i,j;
                 
         for(size_t i = 0 ; i < MAX_INPUTS; i++){
                 printf("Enter the a date(mm/dd/yy): ");
@@ -23,58 +23,36 @@ int main(int argc, char *argv[]){
                 date[i][DAY] = (int)strtol(endptr, &endptr, 10);
                 endptr++;
                 date[i][YEAR] = (int)strtol(endptr, NULL, 10);
-                memset(buffer, '\0', SIZE);
-                counter++;
 
-                if(!(date[i][MONTH] || date[i][DAY] || date[i][YEAR])){
+                if(date[i][MONTH] || date[i][DAY] || date[i][YEAR]){
+                        counter++;
+                }else{
+                        if(counter == 0){
+                                fprintf(stderr,"No date, provided\n");
+                                return EXIT_SUCCESS;
+                        }
                         break;
-                } 
-        }
-        /* DEBUG
-        for(size_t i = 0; i < counter; i++){
-                printf("date %ld\n", i + 1);
-                printf("\t%02d/%02d/%02d\n", date[i][MONTH],
-                                       date[i][DAY],
-                                       date[i][YEAR]);
-        }
-        */
-
-
-
-
-        /* OLD CODE
-        if(year[1] < year[2]){
-               i = 1;
-               j = 2;
-        } else if(year[1] == year[2]){
-                if(month[1] < month[2]){
-                        i = 1;
-                        j = 2;
-                }else if(month[1] == month[2]){
-                       if(day[1] < day[2]){
-                                i = 1;
-                                j = 2;
-                       } else if(day[1] == day[2]){
-                                printf("Both dates are the same\n");
-                                return 1;
-                       } else{
-                               i = 2;
-                               j = 1;
-                       }
-                } else { 
-                        i = 2;
-                        j = 1;
                 }
-        } else {
-                i = 2;
-                j = 1;
+        }
+        int eIndex = 0;
+
+        for(size_t i = 1; i < counter; i++){
+                if(date[i][YEAR] < date[eIndex][YEAR] ||
+                  (date[i][YEAR] == date[eIndex][YEAR] && 
+                   date[i][MONTH] < date[eIndex][MONTH]) || 
+                  (date[i][YEAR] == date[eIndex][YEAR] &&
+                   date[i][MONTH] == date[eIndex][MONTH] &&
+                   date[i][DAY] < date[eIndex][DAY])){
+                        eIndex = i;
+                }
+                                  
         }
 
-        printf("%02d/%02d/%02d is earlier than %02d/%02d/%02d\n", 
-                                                        month[i], day[i], 
-                                                        year[i], month[j],
-                                                        day[j], year[j]);
-                                                        */
+
+        printf("%02d/%02d/%02d is the earliest date\n", 
+                                          date[eIndex][MONTH],
+                                          date[eIndex][DAY],
+                                          date[eIndex][YEAR]);
 
         return 0;
 }
