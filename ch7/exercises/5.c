1159de474bb89bff135a65db414e03dad0864530
Hersonrock <hjpg20@gmail.com>
2024-07-08 00:31:31 -0500

    Finished Exercise 7.5

---
+++ ch7/exercises/5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+        printf("%d\n", 'A');
+        printf("%d\n",0b1000001);
+        printf("%d\n",0101);
+        printf("%d\n",0x41);
+        //All are legal ways to write 65
+        return EXIT_SUCCESS;
+}
