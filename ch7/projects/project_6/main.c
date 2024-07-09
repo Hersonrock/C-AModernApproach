e515e05a0a60911aae7a66a1ab13b7c4df4c4e34
Hersonrock <hjpg20@gmail.com>
2024-07-09 10:39:15 -0500

    Finished Project 7.6

---
+++ ch7/projects/project_6/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+
+        printf("Size of int %lu\n", sizeof(int));
+        printf("Size of short %lu\n", sizeof(short));
+        printf("Size of float %lu\n", sizeof(float));
+        printf("Size of double %lu\n", sizeof(double));
+        printf("Size of long double %lu\n", sizeof(long double));
+
+        return EXIT_SUCCESS;
+}
