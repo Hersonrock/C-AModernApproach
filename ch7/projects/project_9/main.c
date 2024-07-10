c0e1414a01a95f4a9c028b0969925ad3b66624d4
Hersonrock <hjpg20@gmail.com>
2024-07-10 10:23:09 -0500

    Finished Project 7.9

---
+++ ch7/projects/project_9/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+        int i = 3;
+        int j = 5;
+
+        printf("i / j + 'a': %d\n", i / j + 'a'); 
+        //resulting operation is of type int.
+        return EXIT_SUCCESS;
+}
