58e8af60ceb41bb051e0dab2ce95c19a3ebeb02b
Hersonrock <hjpg20@gmail.com>
2024-07-11 11:04:04 -0500

    Finished Project 7.9

--- ch7/projects/project_9/main.c
+++ ch7/projects/project_9/main.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define MAX_SIZE 20
 int main(int argc, char *argv[]){
+        char buffer[MAX_SIZE];
+        int time24, time12, min;
+        char *endptr;
+
+        printf("Enter a 12-hour time: ");
+        fgets(buffer, MAX_SIZE, stdin);
+        time12 = (int)strtol(buffer, &endptr, 10);
+        endptr++; // Skipping ';' character
+        min = (int)strtol(endptr, &endptr, 10);
+        if(*endptr == 'P' || time12 != 12){
+                time24 = time12 + 12;
+        }
+        
+        printf("Equivalent 24-hour time: %d:%d\n", time24, min);
+        
+
         return EXIT_SUCCESS;
 }
